c28058be2f959e2f55af2b4dde843aa99d67b26a
Andrew Yang <a.yang629@Andrews-MacBook-Pro-2.local>
2016-04-08 19:32:11 -0700

    restructured client

--- simple_client.py
+++ simple_client.py
@@ -1,24 +1,33 @@
 import paho.mqtt.client as mqtt
+import sys
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, rc):
-	print("Connected with result code "+str(rc))
-	# Subscribing in on_connect() means that if we lose the connection and
-	# reconnect then subscriptions will be renewed.
-	client.subscribe("$SYS/#")
+class MQTT_Client:
+	def __init__(self):
+		self.client = mqtt.Client()
+		self.client.on_connect = self.on_connect
+		self.client.on_message = self.on_message
+		self.client.connect("localhost", 1883, 60)
+		print "Client connected!"
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-	print(msg.topic+" "+str(msg.payload))
+	def run(self):
+		self.client.loop_forever()
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+	# The callback for when the client receives a CONNACK response from the server.
+	def on_connect(self, client, userdata, rc):
+		print("Connected with result code "+str(rc))
+		# Subscribing in on_connect() means that if we lose the connection and
+		# reconnect then subscriptions will be renewed.
+		client.subscribe("$SYS/#")
 
-client.connect("localhost", 1883, 60)
+	# The callback for when a PUBLISH message is received from the server.
+	def on_message(self, client, userdata, msg):
+		print(msg.topic+" "+str(msg.payload))
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+
+if __name__ == "__main__":
+	client = MQTT_Client()
+	# Blocking call that processes network traffic, dispatches callbacks and
+	# handles reconnecting.
+	# Other loop*() functions are available that give a threaded interface and a
+	# manual interface.
+	client.run()
